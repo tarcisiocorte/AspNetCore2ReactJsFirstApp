44edf38adde19563d42f1d799c03ff3963cdefdf
tarcisiocorte <tarcisiocorte@gmail.com>
2018-05-03 11:32:13 -0300

    added database scripts

---
+++ Database1/Script1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO tblCities VALUES('São Paulo');      
+INSERT INTO tblCities VALUES('Campinas');      
+INSERT INTO tblCities VALUES('Santos');      
+INSERT INTO tblCities VALUES('Natal');      
+INSERT INTO tblCities VALUES('Guiçara');   
---
+++ Database1/tblCities.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE tblEmployee (      
+EmployeeID int IDENTITY(1,1) NOT NULL PRIMARY KEY,      
+Name varchar(20) NOT NULL ,      
+City varchar(20) NOT NULL ,      
+Department varchar(20) NOT NULL ,      
+Gender varchar(6) NOT NULL       
+)      
+GO      
+      
+CREATE TABLE tblCities (      
+CityID int IDENTITY(1,1) NOT NULL PRIMARY KEY,      
+CityName varchar(20) NOT NULL       
+)      
+GO  
