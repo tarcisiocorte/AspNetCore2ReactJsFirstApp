d9b8222e8fe601ea8dfdae04ea286bc9a20afeef
tarcisiocorte <tarcisiocorte@gmail.com>
2018-05-03 15:08:27 -0300

    change layout

--- WebApplication1/ClientApp/routes.tsx
+++ WebApplication1/ClientApp/routes.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { FetchEmployee } from './components/FetchEmployee';
+import { AddEmployee } from './components/AddEmployee';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
-</Layout>;
+    <Route exact path='/' component={Home} />
+    <Route path='/fetchemployee' component={FetchEmployee} />
+    <Route path='/addemployee' component={AddEmployee} />
+    <Route path='/employee/edit/:empid' component={AddEmployee} />
+</Layout>;  
